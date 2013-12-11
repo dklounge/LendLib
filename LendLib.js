bd6dad75ba0b028affd3ac4f4cdace914c7d22fc
David Kim <daviddarden11@gmail.com>
2013-12-11 08:16:13 -0800

    updated app and installed bootstrap

--- LendLib.js
+++ LendLib.js
@@ -1,17 +1,21 @@
-var lists = new Meteor.Collection("Lists");
+Lists = new Meteor.Collection("Lists");
 
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "my list.";
-  };
+  // Template.hello.greeting = function () {
+  //   return "my list.";
+  // };
 
-  Template.hello.events({
-    'click input' : function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
+  // Template.hello.events({
+  //   'click input' : function () {
+  //     // template data, if any, is available in 'this'
+  //     if (typeof console !== 'undefined')
+  //       console.log("You pressed the button");
+  //   }
+  // });
+
+  Template.categories.Lists = function () {
+    return Lists.find({}, {sort: {category: 1}});
+  };
 }
 
 if (Meteor.isServer) {
